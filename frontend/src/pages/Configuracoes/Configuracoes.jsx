197badaee5eaebf4df56adfdb82d4fe76dba9042
Luiz Guandalini <luizluizcorinthians@gmail.com>
2025-09-27 09:58:14 -0300

    correcao de bug que fazia as opcoes da sidebar estrepolarem a mesma ao passar o mouse

--- frontend/src/pages/Configuracoes/Configuracoes.jsx
+++ frontend/src/pages/Configuracoes/Configuracoes.jsx
@@ -5,13 +5,6 @@ import './Configuracoes.css';
 const Configuracoes = ({ toastConfig }) => {
   return (
     <div className="configuracoes-page">
-      <div className="page-header">
-        <h1 className="page-title">Configurações do Sistema</h1>
-        <p className="page-description">
-          Gerencie as configurações da aplicação
-        </p>
-      </div>
-      
       <div className="page-content">
         <ConfiguracoesSection toastConfig={toastConfig} />
       </div>
